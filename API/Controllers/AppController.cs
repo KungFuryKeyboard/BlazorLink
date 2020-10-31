101743b15d65f9a98ad5b1a6aa82abdc70564b8e
KungFuryKeyboard <ry4ndevwork@gmail.com>
2020-10-31 23:49:23 +0000

    #3

---
+++ API/Controllers/AppController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Model;
+using API.Domain;
+using Microsoft.Extensions.Options;
+
+namespace API.Controllers
+{
+    [ApiController]
+   [Route("")]
+    public class AppController : ControllerBase
+    {
+
+
+        private readonly ILogger<AppController> _logger;
+         private readonly ApplicationDbContext _db;
+         private readonly IOptions<AppConfigOptions> _config;
+
+        public AppController(ILogger<AppController> logger, ApplicationDbContext db, IOptions<AppConfigOptions> config)
+        {
+            _logger = logger;
+           _db = db;
+            _config = config;
+        }
+
+
+
+
+      [HttpPost]
+      public ActionResult<string> Post([FromBody] NewURLRequest urlToShorten)
+      {
+        if(!String.IsNullOrEmpty(urlToShorten.Url)){
+
+              var newUrl = new ShortUrl();
+              newUrl.DestinationURL = urlToShorten.Url;
+              string UrlToken = generateToken(7);
+              newUrl.Id = UrlToken;
+              _db.ShortUrl.Add(newUrl);
+             _db.SaveChanges();
+            string ReturnUrl = _config.Value.DefaultDomain + "/" + UrlToken;
+            Console.WriteLine(ReturnUrl);
+
+         return ReturnUrl;
+
+        }else{
+            return BadRequest("invalid url");
+        }
+
+      }
+      private string generateToken(int Length){
+         Guid id = Guid.NewGuid();
+         Console.WriteLine($"Guid : {id}");
+         var UrlToken = Convert.ToBase64String(id.ToByteArray())
+             .Replace("_", "")
+             .Replace("/", "")
+             .Replace("+", "")
+             .Replace("-", "")
+             .Substring(0, Length);
+         Console.WriteLine($"base 64 : {UrlToken}");
+
+         Console.WriteLine($"Shuffle base 64 : {UrlToken}");
+         return UrlToken;
+      }
+
+
+        // [HttpGet]
+        // public IEnumerable<WeatherForecast> Get()
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -27,7 +27,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddOptions<AppConfigOptions>().Bind(Configuration.GetSection(AppConfigOptions.AppConfig));
             services.AddControllers();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase(databaseName: "ShortURl"));
             services.AddSwaggerGen(c =>
