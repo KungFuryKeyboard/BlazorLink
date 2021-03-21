25dfdea0336eecae20a455b13509b6e06d522f96
KungFuryKeyboard <ry4ndevwork@gmail.com>
2021-03-21 23:50:41 +0000

    text changes

--- WebUI/Model/URLLink.cs
+++ WebUI/Model/URLLink.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using System.ComponentModel.DataAnnotations;
 
 namespace WebUI.Model
@@ -9,7 +6,7 @@ namespace WebUI.Model
     public class URLLink
     {
         [Required]
-        [StringLength(180,MinimumLength =4, ErrorMessage = "URL too incorrect length (4 to 180 character limit).")]
+        [StringLength(180,MinimumLength =4, ErrorMessage = "URL incorrect length (4 to 180 character limit).")]
 
         public string URL { get; set; }
     }
